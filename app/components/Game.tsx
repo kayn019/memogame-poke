5d88ea434c54de6521e9aaea3c654d9e7ed60627
kay019 <kayn01191@gmail.com>
2024-03-29 00:27:44 +0700

    feat: endless main functionality

--- app/components/Game.tsx
+++ app/components/Game.tsx
@@ -14,6 +14,7 @@ export default function Game({ mode }: GameProps) {
   const [best, setBest] = useState<number>(0);
   const [isWin, setIsWin] = useState(false);
   let pokeToShow = 0;
+  let [pokeToShowUS, setPokeToShowUS] = useState<number>(0);
 
   let [selPokemon, setSelPokemon] = useState<number[]>([]);
 
@@ -49,18 +50,16 @@ export default function Game({ mode }: GameProps) {
   }
 
   useEffect(() => {
-    // const newPoke = [];
-    // for (let i = 0; i < pokeToShow; i++) {
-    //   const pokeNum = Math.floor(Math.random() * 721);
-    //   newPoke.push(pokeNum);
-    // }
-    // setPokemon(newPoke);
+    if(pokeToShowUS == 0){
+      setPokeToShowUS(5);
+      
+    }
+  });
 
-    // newPoke.forEach((parameter) => {
-    //   getPokemon(parameter);
-    // });
+  useEffect(() => {
     setSelPokemon([]);
     reshufflePokemon();
+  
   }, []); // Empty dependency array to run this effect only once
 
   // useEffect to render PokeCard when all data is fetched
@@ -68,55 +67,59 @@ export default function Game({ mode }: GameProps) {
   //   if (pokemonData.length === pokeToShow) {
   //     console.log("All data fetched:", pokemonData);
   //     console.log(pokemon);
+  //     console.log(selPokemon);
   //   }
-  // }, [pokemonData, pokeToShow]);
-
-  const handleCardClick = (id: number) => {
-    // Do something with the clicked index, such as showing details or updating state
-    console.log("Clicked card id:", id);
-    console.log(selPokemon);
-    if (selPokemon.includes(id) == true) {
-      //lose
-      toggleModal();
-      setIsWin(false);
-    } else {
-      setSelPokemon((prevData: any) => [...prevData, id]);
-      setScore(score + 1);
-      
-      //reshuffle
-      reshufflePokemon();
-    }
-  };
+  // }, [pokemonData, pokeToShow, selPokemon]);
 
   useEffect(() => {
-    console.log("useefefcalled");
+
     if (score > best) {
       setBest(score);
     }
+
+    if (mode !== "Endless" && score === pokeToShow) {
+      setIsWin(true);
+      toggleModal();
+    }
     
-      if(score == pokeToShow){
-        setIsWin(true);
-        toggleModal();
-      }
-    
-  },[score]);
+  }, [score, pokeToShow]);
+
+  
 
   const reshufflePokemon = () => {
     let newPoke: number[] = [];
-    if (pokemonData.length == pokeToShow) {
-      //if this is not the first time
-      newPoke = pokemon;
-      console.log("not first");
-    } else {
-      // this is the first time initialized
-      console.log("new");
-      newPoke = selPokemon; //get how many pokemon already selected
-      const newRandomPoke = pokeToShow - newPoke.length; //get how many more new random poke to fill
-      for (let i = 0; i < newRandomPoke; i++) {
+    if (mode === "Endless") {
+      
+      // For endless mode, keep adding more Pokemon
+      newPoke = selPokemon.slice();
+      
+      
+      if(mode == "Endless" && score > pokeToShow - 1){
+        pokeToShow = score + 5;
+        
+      }
+      while (newPoke.length < (pokeToShowUS == 0 ?  5 : pokeToShow)) {
         const pokeNum: any = getRandomNum(newPoke);
         newPoke.push(pokeNum);
+      }}else{
+        if (pokemonData.length == pokeToShow) {
+          //if this is not the first time
+          newPoke = pokemon;
+      
+        } else {
+          // this is the first time initialized
+          console.log("new");
+          newPoke = selPokemon; //get how many pokemon already selected
+          const newRandomPoke = pokeToShow - newPoke.length; //get how many more new random poke to fill
+          for (let i = 0; i < newRandomPoke; i++) {
+            const pokeNum: any = getRandomNum(newPoke);
+            newPoke.push(pokeNum);
+          }
+        }
       }
-    }
+    
+
+    // setPokeToShowUS(pokeToShow);
     newPoke.sort(() => Math.random() - 0.5);
     setPokemon(newPoke);
     // Clear existing Pokemon data
@@ -130,48 +133,64 @@ export default function Game({ mode }: GameProps) {
   // wont get same pokemon number twice
   function getRandomNum(newPoke: number[]) {
     const temp = Math.floor(Math.random() * 721);
-    if (newPoke.includes(temp) == true) {
-      getRandomNum(newPoke);
+    if (!newPoke.includes(temp)) {
+      return temp;
     } else {
-      return Math.floor(Math.random() * 721);
+      return getRandomNum(newPoke);
     }
   }
 
+  const handleCardClick = (id: number) => {
+    // Do something with the clicked index, such as showing details or updating state
+    console.log("Clicked card id:", id);
+    console.log(selPokemon);
+    if (selPokemon.includes(id) == true) {
+      //lose
+      toggleModal();
+      setIsWin(false);
+    } else {
+      setSelPokemon((prevData: any) => [...prevData, id]);
+      setScore(score + 1);
+
+      //reshuffle
+      reshufflePokemon();
+    }
+  };
+
   const handleGameOver = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
+
     window.location.reload();
   };
 
   const handleOnRestart = () => {
     toggleModal();
-  setSelPokemon([]); // Clear selected Pokemon
-  setScore(0); // Reset score
+    setSelPokemon([]); // Clear selected Pokemon
+    setScore(0); // Reset score
 
-  
-  // Reshuffle and fetch new Pokemon from pokemonData
-  const newPoke: number[] = [];
-  const newRandomPoke = pokeToShow;
-  for (let i = 0; i < newRandomPoke; i++) {
-    const pokeNum: any = getRandomNum(newPoke);
-    newPoke.push(pokeNum);
-  }
-  newPoke.sort(() => Math.random() - 0.5);
-  setPokemon(newPoke);
+    // Reshuffle and fetch new Pokemon from pokemonData
+    const newPoke: number[] = [];
+    const newRandomPoke = pokeToShow;
+    for (let i = 0; i < newRandomPoke; i++) {
+      const pokeNum: any = getRandomNum(newPoke);
+      newPoke.push(pokeNum);
+    }
+    newPoke.sort(() => Math.random() - 0.5);
+    setPokemon(newPoke);
 
-  // Clear existing Pokemon data
-  setPokemonData([]);
+    // Clear existing Pokemon data
+    setPokemonData([]);
 
-  newPoke.forEach((parameter) => {
-    getPokemon(parameter);
-  });
+    newPoke.forEach((parameter) => {
+      getPokemon(parameter);
+    });
   };
 
   return (
     <>
       <div className="flex min-h-screen w-full h-full flex-col content-start items-center justify-between px-10 py-5">
         <div className="flex flex-row justify-between w-full mb-4">
-          <button className="bg-white rounded-2xl px-5 py-2 hover:bg-gray-100" >
+          <button className="bg-white rounded-2xl px-5 py-2 hover:bg-gray-100">
             Back
           </button>
           <div>Don't click the same pokemon twice!</div>
@@ -182,7 +201,9 @@ export default function Game({ mode }: GameProps) {
         </div>
 
         <div className="grid grid-cols-5 auto-cols-max   w-full h-full overflow-visible gap-5">
-          {pokemonData.length === pokeToShow && (
+         
+          {pokemonData.length >= pokeToShowUS && (
+            
             <PokeCard
               pokemon={pokemon}
               pokemonData={pokemonData}
--- app/components/PokeCard.tsx
+++ app/components/PokeCard.tsx
@@ -1,26 +1,30 @@
 
+
 interface PokeCardProps{
     pokemon:number[];
     pokemonData:any;
     onClick:Function;
 }
 
-export default function PokeCard({pokemon, pokemonData, onClick}:PokeCardProps) {
-
-    `https://pokeapi.co/api/v2/pokemon/{number}/`
-    console.log(pokemonData);
-    console.log(pokemon);
-    return(
-        <>
-            {pokemon.map((num, index) => {
-              
-                return(
-                  <div className=" w-200px h-400px  bg-white hover:bg-gray-100 rounded-xl grid justify-center content-center shadow-xl " key={pokemonData[index].id} onClick={() => onClick(pokemonData[index].id)}>
-                    <div className="text-center">{pokemonData[index].species.name}</div>
-                    <img className="max-w-max h-auto" src={pokemonData[index].sprites.front_default} alt="" />
-                  </div>   )
-                })}
-        </>
-       
-    )
+export default function PokeCard({ pokemon, pokemonData, onClick }: PokeCardProps) {
+  return (
+    <>
+      {pokemon.map((num, index) => {
+        const data = pokemonData.find((poke: any) => poke.id === num);
+        if (!data) {
+          return <div key={"wait" + index} className="w-200px h-400px bg-white hover:bg-gray-100 rounded-xl grid justify-center content-center shadow-xl ">
+                  
+                    <img src="/loading.gif" alt="" />
+                
+                </div>; // If data is not available yet, 
+        }else{
+        return (
+          <div className="w-200px h-400px bg-white hover:bg-gray-100 rounded-xl grid justify-center content-center shadow-xl min-h-full" key={data.id} onClick={() => onClick(data.id)}>
+            <div className="text-center">{data.species.name}</div>
+            <img className="max-w-max h-auto" src={data.sprites.front_default || 'https://via.placeholder.com/150'} alt="" />
+          </div>
+        );}
+      })}
+    </>
+  );
 }
